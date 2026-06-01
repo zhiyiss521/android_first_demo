f3af932898b0cf9f01476c709b4006bd039b19d9
Kai Zhang <1018476287@qq.com>
2026-06-01 20:19:40 +0800

    fix: 完善详情页

--- app/src/main/java/com/zhiyi/android_first_demo/ui/activity/UserDetailActivity.kt
+++ app/src/main/java/com/zhiyi/android_first_demo/ui/activity/UserDetailActivity.kt
@@ -1,13 +1,17 @@
 package com.zhiyi.android_first_demo.ui.activity
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.appbar.AppBarLayout
 import com.zhiyi.android_first_demo.R
 import com.zhiyi.android_first_demo.databinding.ActivityPostDetailBinding
 import com.zhiyi.android_first_demo.databinding.ActivityUserDetailBinding
@@ -28,7 +32,7 @@ class UserDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
         window.statusBarColor = android.graphics.Color.TRANSPARENT
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_user_detail)
@@ -41,23 +45,69 @@ class UserDetailActivity : AppCompatActivity() {
     }
 
     fun initUI(){
+        setAppBar()
 
         binding.btnBack.setOnClickListener( {
             finish()
         })
-
         binding.rvPhotos.layoutManager = StaggeredGridLayoutManager(
             2,
             StaggeredGridLayoutManager.VERTICAL
         )
         binding.rvPhotos.adapter = adapter
-
-
         lifecycleScope.launch {
             vm.userState.collect { user ->
                 adapter.submitList(user?.photos)
             }
         }
+    }
+
+    fun setAppBar(){
+        binding.appBar.addOnOffsetChangedListener(
+            AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+
+                val total = appBarLayout.totalScrollRange
+
+                val percent = kotlin.math.abs(verticalOffset) / total.toFloat()
+
+                if (percent > 0.75f) {
+
+                    // 吸顶状态
+                    binding.layoutCollapsedUser.animate()
+                        .alpha(1f)
+                        .setDuration(180)
+                        .start()
+
+                    binding.layoutCollapsedUser.visibility = View.VISIBLE
+
+                    binding.cardBack.setCardBackgroundColor(Color.WHITE)
+
+                    binding.cardBack.cardElevation = 6f
+
+                    binding.btnBack.imageTintList =
+                        ColorStateList.valueOf(Color.BLACK)
+
+                } else {
+
+                    // 展开状态
+                    binding.layoutCollapsedUser.animate()
+                        .alpha(0f)
+                        .setDuration(180)
+                        .start()
+
+                    binding.layoutCollapsedUser.visibility = View.INVISIBLE
+
+                    binding.cardBack.setCardBackgroundColor(
+                        Color.parseColor("#66000000")
+                    )
+
+                    binding.cardBack.cardElevation = 0f
+
+                    binding.btnBack.imageTintList =
+                        ColorStateList.valueOf(Color.WHITE)
+                }
+            }
+        )
     }
 
     fun getData(){
